307a525205017e4dca96d33c1af063ca6b8769e7
Juubes <grumpt1@gmail.com>
2021-01-14 08:55:26 +0200

    spec komento valmis

--- src/dtmproject/commands/SpectateCommand.java
+++ src/dtmproject/commands/SpectateCommand.java
@@ -1,13 +1,12 @@
 package dtmproject.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import dtmproject.DTM;
-import dtmproject.data.DTMPlayerData;
-import dtmproject.logic.GameState;
 
 public class SpectateCommand implements CommandExecutor {
 	private final DTM pl;
@@ -18,17 +17,28 @@ public class SpectateCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (!(sender instanceof Player)) {
-			sender.sendMessage("§eBruhh.");
+		if (args.length == 0) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("§eBruhh.");
+				return true;
+			}
+
+			Player p = (Player) sender;
+			pl.getLogicHandler().getCurrentMap().sendToSpectate(p);
+
+			p.sendMessage("§eOlet nyt katsoja.");
 			return true;
 		}
 
-		Player p = (Player) sender;
-		DTMPlayerData pd = pl.getDataHandler().getPlayerData(p);
-		
-		pl.getLogicHandler().getCurrentMap().sendToSpectate(p);
+		Player target = Bukkit.getPlayer(args[0]);
+		if (target == null) {
+			sender.sendMessage("§ePelaajaa ei löytynyt.");
+			return true;
+		}
 
-		p.sendMessage("§eOlet nyt katsoja.");
+		pl.getLogicHandler().getCurrentMap().sendToSpectate(target);
+		sender.sendMessage("§eLähetetty " + target.getName() + " katsojaksi.");
+		target.sendMessage("§eSinut on lähetetty katsojaksi.");
 		return true;
 	}
 
